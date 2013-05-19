87991b89bf69d41d14545234983e9642b7daf6ed
Christopher Martin <ch.martin@gmail.com>
2013-05-18 23:28:47 -0400

    301 on trailing slash

--- web/app/controllers/Application.scala
+++ web/app/controllers/Application.scala
@@ -5,6 +5,10 @@ import play.api.mvc._
 
 object Application extends Controller {
 
+  def untrail(path: String) = Action {
+    MovedPermanently("/%s".format(path))
+  }
+
   def index = Action {
     Ok(views.html.index())
   }
